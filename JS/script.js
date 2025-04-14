ec0c3ddb41aad7e6003f3a5acadfd7b99d4bac96
GiovanniGarlaschini22 <giovannigarlaschini.boolean@gmail.com>
2025-04-14 15:31:57 +0200

    Feat: procedimento scritto nei commenti

---
+++ JS/script.js
@@ -0,0 +1,41 @@
+// CONSEGNA //
+
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va stampato in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+// Questo richiederà un minimo di ricerca.
+
+// FINE CONSEGNA //
+
+
+// RACCOLTA DATI //
+
+// chiedere all'utente il numero di chilometri che vuole percorrere;
+// chiedere all'utente l'età del passeggero;
+
+// FINE RACCOLTA DATI //
+
+
+// ESECUZIONE LOGICA //
+
+/**
+ * chiedere all'utente il numero di chilometri che vuole percorrere
+ * chiedere all'utente l'età del passeggero
+ 
+ * se il passeggero é minorenne
+ *      applico uno sconto del 20%
+  
+ * se il passeggero é maggiorenne under 65
+ *      paga il biglietto intero (0,21€ al km)
+ 
+ * se il passeggero é maggiorenne over 65
+ *      applico uno sconto del 40%
+*/
+
+// FINE ESECUZIONE LOGICA //
+
+
+// OUTPUT //
